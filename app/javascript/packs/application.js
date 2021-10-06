698b2b27a91d27fc8a2aee749c90efd8b2e83834
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-10-05 22:29:44 -0700

    jquery setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap"
+import './src/application.scss'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("bootstrap/dist/js/bootstrap")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-import "packs/custom"
-import "packs/posts.js"
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
 
