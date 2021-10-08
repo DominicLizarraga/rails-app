6bd0a1ee577d91f89b7de661464b06933b5903f0
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-10-07 20:43:50 -0700

    added some migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_052715) do
+ActiveRecord::Schema.define(version: 2021_10_08_034002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
